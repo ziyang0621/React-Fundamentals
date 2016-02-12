c7b8c433eb0aae572f08fd08ba0c4d5694a3a836
Ziyang tan <ziyang0621@gmail.com>
2016-02-12 14:48:50 -0800

    Build a JSX Live Compiler

--- App.js
+++ App.js
@@ -4,42 +4,43 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {data: [
-      {id: 1, name: "Simon Bailey"},
-      {id: 2, name: "Thomas Bureson"},
-      {id: 3, name: "Will Button"},
-      {id: 4, name: "Ben Clinkbeard"},
-      {id: 5, name: "Kent Dodds"},
-      {id: 6, name: "Trevor Ewen"},
-      {id: 7, name: "Aaron Frost"},
-      {id: 8, name: "Joel Hooks"},
-      {id: 9, name: "Jafar Husain"},
-      {id: 10, name: "Tim Kindberg"},
-      {id: 11, name: "John Lindquist"},
-      {id: 12, name: "Joe Maddalone"},
-      {id: 13, name: "Tyler McGinnis"},
-      {id: 14, name: "Scott Moss"},
-      {id: 15, name: "Robert Penner"},
-      {id: 16, name: "Keith Peters"},
-      {id: 17, name: "Lukas Ruebbelke"},
-      {id: 18, name: "Brett, Shollenberger"}
-    ]}
+    this.state={
+      input: '/* add your jsx here*/',
+      output: '',
+      err: ''
+    }
+    this.update = this.update.bind(this);
+  }
+  update(e) {
+    let code = e.target.value;
+    try {
+      this.setState({
+        output: babel.transform(code, {
+          stage: 0,
+          loose: 'all'
+        }).code,
+        err: ''
+      })
+    }
+    catch (err) {
+      this.setState({
+        err: err.message
+      })
+    }
   }
   render() {
-    let rows = this.state.data.map(person => {
-      return <PersonRow key={person.id} data={person} />
-    })
-    return <table>
-      <tbody>{rows}</tbody>
-    </table>
+    return (
+      <div>
+        <header>{this.state.err}</header>
+        <div className="container">
+          <textarea onChange={this.update} defaultValue={this.state.input}></textarea>
+          <pre>
+            {this.state.output}
+          </pre>
+        </div>
+      </div>
+    )
   }
-}
-
-const PersonRow = (props) => {
-  return <tr>
-    <td>{props.data.id}</td>
-    <td>{props.data.name}</td>
-  </tr>
 }
 
 export default App
